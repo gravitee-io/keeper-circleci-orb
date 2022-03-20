0fb2ceca68e94f5b2164291c3cbec9834ca354c4
Julien Giovaresco <dev@giovaresco.fr>
2022-03-20 11:01:37 +0100

    feat: add env-export command

---
+++ src/scripts/env-export/env-export.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o errexit  # Exit when simple command fails               'set -e'
+set -o errtrace # Exit on error inside any functions or subshells.
+set -o pipefail # Do not hide errors within pipes              'set -o pipefail'
+
+generate_random_heredoc_identifier() {
+  cat /dev/urandom | env LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1
+}
+
+get_secret_from_ksm() {
+  url=$1
+  ksm secret notation "${url}"
+}
+
+EnvExport() {
+  if [ "x${SECRET_ENV_NAME}" == "x" ]; then
+    >&2 echo "SECRET_ENV_NAME env var not set"
+    exit 1
+  fi
+
+  if [ "x${SECRET_URL}" == "x" ]; then
+    >&2 echo "SECRET_URL env var not set"
+    exit 1
+  fi
+
+  cd /tmp
+
+  # To support multiline secrets, we'll use the heredoc syntax to populate the environment variables.
+  # As the heredoc identifier, we'll use a randomly generated 64-character string,
+  # so that collisions are practically impossible.
+  random_heredoc_identifier=$(generate_random_heredoc_identifier) || true
+  SECRET=$(get_secret_from_ksm "${SECRET_URL}")
+
+  {
+    printf "export %s=\$(cat <<%s\n" "${SECRET_ENV_NAME}" "${random_heredoc_identifier}"
+    printf "%s\n" "${SECRET}"
+    printf "%s\n)\n" "${random_heredoc_identifier}"
+  } >> "${BASH_ENV}"
+}
+
+# Will not run if sourced from another script.
+# This is done so this script may be tested.
+if [ "${0#*$TEST_ENV}" == "$0" ]; then
+    EnvExport
+fi
