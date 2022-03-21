a41c4f6b6cd2fcb760bb7fb5ea1e3d0ce93ebe80
Julien Giovaresco <dev@giovaresco.fr>
2022-03-21 10:42:34 +0100

    chore: put generated commitlint config in the existing file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "body-max-line-length": [1, "always", 100],
+        "subject-case": [1, "always", "lower-case"],
+        "subject-empty": [1, "never"],
+        "type-empty": [1, "never"],
+    },
 }
